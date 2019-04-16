f38fc71910d04596cf60d72ff5b8c1d7eb50c1a6
Ram Kumar <ramkumar222014@gmail.com>
2019-04-17 02:04:13 +0530

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ram Kumar on 15/04/19.
+//  Copyright © 2019 Ram Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //@IBOutlet weak var barChartView: BarChartView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
