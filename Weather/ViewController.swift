6b6e4602c463725977a55e380ea5372af3a93687
Ram Kumar <ram.kumar@gaussnetworks.com>
2019-04-18 18:34:33 +0530

    hui

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -12,11 +12,35 @@ import Charts
 class ViewController: UIViewController {
 
     @IBOutlet weak var barChartView: BarChartView!
+    var months: [String]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let xAxis = barChartView.xAxis
+        xAxis.labelPosition = .bottom
+        xAxis.labelFont = .systemFont(ofSize: 10)
+        xAxis.granularity = 1
+        xAxis.labelCount = months.count
+        xAxis.valueFormatter = DayAxisValueFormatter(chart: barChartView)
+        
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        setChart(dataPoints:months, values: unitsSold)
     }
 
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart."
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y:values[i],data:dataPoints[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        barChartView.data = chartData
+    }
 
 }
 
